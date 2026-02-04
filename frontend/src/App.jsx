83ff52990ee180f985093829f212efd02902766b
aadthi-B <aadthi2004@gmail.com>
2026-02-04 19:26:45 +0530

    header component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>TalkingGlove</h1>
+      <footer />
     </>
   )
 }
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,6 @@
+function header(){
+    return(
+        <h1>Talking Glove</h1>
+    );
+}
+export default header;
